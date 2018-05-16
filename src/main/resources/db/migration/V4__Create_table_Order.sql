a35e18068e6862367aab43ae9bf3b730499a0746
cmy14217 <18796682719@163.com>
2018-05-16 11:29:47 +0800

    create table to store the info of order

--- src/main/resources/db/migration/V4__Create_table_Order.sql
+++ src/main/resources/db/migration/V4__Create_table_Order.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Orders{
+CREATE TABLE Orders(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     totalPrice FLOAT,
-    status VARCHAR(10) DEFAULT "unpaid",
-    createTime DATE DEFAULT CURRENT_DATE,
+    status VARCHAR(30) DEFAULT "unpaid",
+    createTime DATE,
     finishTime DATE DEFAULT NULL,
     paidTime DATE DEFAULT NULL,
     withdrawnTime DATE DEFAULT NULL,
     userId INT NOT NULL,
-    FOREIGN KEY(userId) REFERENCES User(id),
-}
+    FOREIGN KEY(userId) REFERENCES Users(id)
+);
