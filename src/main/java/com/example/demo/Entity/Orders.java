9312d2fe2e67b1c23500dc6febcf1735f4748cac
cmy14217 <18796682719@163.com>
2018-05-16 11:39:40 +0800

    Create Entity of Orders,which has OneToMany realtionship with ProductSnapshot and OneToOne relationship with LogisticsRecord and ManyToOne relationship with User

---
+++ src/main/java/com/example/demo/Entity/Orders.java
@@ -0,0 +1,110 @@
+package com.example.demo.Entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Orders {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Float totalPrice;
+    private String status;
+    private Date createTime;
+    private Date finishTime;
+    private Date paidTime;
+    private Date withdrawnTime;
+    private Integer userId;
+
+    @OneToMany(mappedBy = "order")
+    private List<ProductSnapshot> products = new ArrayList<>();
+
+    @OneToOne(mappedBy = "order")
+    private LogisticsRecord logisticsRecord;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    public Orders() {
+    }
+
+    public Orders(Float totalPrice, Integer userId) {
+        this.totalPrice = totalPrice;
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(Date finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public Date getPaidTime() {
+        return paidTime;
+    }
+
+    public void setPaidTime(Date paidTime) {
+        this.paidTime = paidTime;
+    }
+
+    public Date getWithdrawnTime() {
+        return withdrawnTime;
+    }
+
+    public void setWithdrawnTime(Date withdrawnTime) {
+        this.withdrawnTime = withdrawnTime;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public List<ProductSnapshot> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductSnapshot> products) {
+        this.products = products;
+    }
+}
