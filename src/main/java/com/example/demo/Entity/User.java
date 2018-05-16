809862bde42225df44b419e8d77307e8717528ca
cmy14217 <18796682719@163.com>
2018-05-16 11:38:18 +0800

    Create Entity of User,which has OneToMany realtionship with Order

---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,50 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Orders> orders = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Orders> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Orders> orders) {
+        this.orders = orders;
+    }
+}
