24bfe3c16a7dbff9667e132bb32c0328a2ccd8e8
cmy14217 <18796682719@163.com>
2018-05-16 11:37:03 +0800

    Create Entity of Product,which has OneToOne realtionship with Inventory

---
+++ src/main/java/com/example/demo/Entity/Product.java
@@ -0,0 +1,68 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Product { //商品信息
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private Float price;
+
+    @OneToOne(mappedBy = "product")
+    private Inventory inventory;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, Float price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+}
