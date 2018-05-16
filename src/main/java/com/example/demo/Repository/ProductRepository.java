6d35a7b7a76a3153a23e2e18166f1dfa09f44b09
cmy14217 <18796682719@163.com>
2018-05-16 19:52:46 +0800

    define the interface to visit table Product

---
+++ src/main/java/com/example/demo/Repository/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+    //create product
+    Product saveAndFlush(Product product);
+
+    //update product
+    @Modifying
+    @Query(value = "update Product set name=?1,description=?2,price=?3 where id=?4")
+    void updateProduct(String name,String description,Float price,Integer id);
+
+    //find product by id
+    Optional<Product> findById(Integer integer);
+
+    //find product by name and description
+    List<Product> findByNameContainingAndDescriptionContaining(String name,String description);
+
+}
