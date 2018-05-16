f4df70d96455456a62ed7d0be96a84dbef86d655
cmy14217 <18796682719@163.com>
2018-05-16 11:37:42 +0800

    Create Entity of Inventory,which has OneToOne realtionship with product

---
+++ src/main/java/com/example/demo/Entity/Inventory.java
@@ -0,0 +1,43 @@
+package com.example.demo.Entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Inventory {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer count;
+    private Integer lockedCount;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Product product;
+
+    public Inventory() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getLockedCount() {
+        return lockedCount;
+    }
+
+    public void setLockedCount(Integer lockedCount) {
+        this.lockedCount = lockedCount;
+    }
+}
